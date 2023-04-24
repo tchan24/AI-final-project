662fd119d1732954306424659a7617c26b4a25d5
tchan24 <tchan0524@gmail.com>
2023-04-23 22:49:14 -0500

    modified:   search.py

--- search.py
+++ search.py
@@ -125,7 +125,7 @@ def breadthFirstSearch(problem: SearchProblem):
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    return genericSearch2(problem, None)
+    return genericSearch(problem, None)
 
 def nullHeuristic(state, problem=None):
     """
